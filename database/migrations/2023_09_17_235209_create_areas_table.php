d37c191cc6dc51b6d028e57109c8381d57fbcbdd
KaiTakizawa <GitHub>
2023-09-19 16:08:14 +0900

    first commit

---
+++ database/migrations/2023_09_17_235209_create_areas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->string('detail', 500);
+            $table->string('address', 100);
+            $table->string('access', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+};
---
+++ database/migrations/2023_09_17_235826_create_reviews_tables.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews_tables', function (Blueprint $table) {
+            $table->id();
+            $table->user_id();
+            $table->area_id();
+            $table->string('comment', 800);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews_tables');
+    }
+};
