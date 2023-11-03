58ce4f90f6f9d8bb72f9dc472512c244758dfe9d
KaiTakizawa <GitHub>
2023-11-03 22:55:00 +0900

    ルーティング見直し

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,15 @@ use App\Http\Controllers\LikeController;
 |
 */
 
-
-Route::get('/', function () {return view('home');})->name('home');
-Route::get('/dashboard', function () {return view('home');})->name('home');
-Route::get('/areas', [AreaController::class, 'area'])->name('area');
-
-
-
-
-
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/', function () {return view('home');})->name('home');
+    Route::get('/dashboard', function () {return view('home');})->name('home');
+    Route::get('/areas', [AreaController::class, 'area'])->name('area');
+
     
     
     Route::post('/areas/search', [AreaController::class, 'search']);
